0382cc0452e29f7ca1763f2dda34dc15e81214e3
Toni Takala <toni.takala@idd.fi>
2021-07-12 12:52:17 +0300

    2.1.1 notifications new koho endpoint

--- src/methods/notification.methods.ts
+++ src/methods/notification.methods.ts
@@ -4,25 +4,24 @@ import { Notification, NotificationProperties } from '../resources/notification.
 
 export class NotificationMethods extends Methods {
   constructor (helper: KohoApiHelper) {
-    super(helper, 'customers/notifications', Notification);
+    super(helper, 'notifications', Notification);
   }
 
   /**
    * Get all customer targeted notifications
    */
   async getAll(params: { start_date?: string; end_date?: string; [propName: string]: any; } = {}) : Promise<Notification[]> {
-    return await super.getAll(params);
+    
+    // force object_type to Customer for backwards compatibility
+    return await super.getAll({ ...params, object_type : 'Customer' });
   }
 
   /**
    * Get all offer targeted notifications (from different endpoint)
    */
   async getAllOfferNotifications(params: { start_date?: string; end_date?: string; [propName: string]: any; } = {}) : Promise<Notification[]> {
-    const result = await this.request('offers/notifications', 'GET', null, params);
-
-    const resources = result.map((r: any) => new Notification(r, this._helper()));
-
-    return resources;
+    // force object_type to NOffer for backwards compatibility
+    return await super.getAll({ ...params, object_type : 'Offer' });
   }
 
   /**
@@ -34,11 +33,8 @@ export class NotificationMethods extends Methods {
     return;
   }
 
-  /**
-   * Not supported for notifications - use updateByCustomerId instead
-   */
   async updateById(id: number, properties: Partial<NotificationProperties>) : Promise<void> {
-    throw new Error(`updateById method not supported for notifications`);
+    return await super.updateById(id, properties);
   }
 
   async updateByCustomerAndNotificationId(customerId: number, notificationId: number, properties: Partial<NotificationProperties>) {
@@ -50,29 +46,8 @@ export class NotificationMethods extends Methods {
     return;
   }
 
-  async updateByOfferAndNotificationId(offerId: number, notificationId: number, properties: Partial<NotificationProperties>) {
-    await this._helper().request(`${this._helper().options.url}/offers/${offerId}/notifications`, 'PUT', {
-      notification_id : notificationId,
-      ...this._generateProperties(properties)
-    });
-
-    return;
-  }
-
    async getById(id: number) : Promise<Notification> {
-    const notifications = await this._helper().request(`${this._helper().options.url}/customers/notifications`, 'GET', null, {
-      notification_id : id
-    });
-
-    if ( ! notifications.length) {
-      throw new Error(`Could not find notification with ID ${id}`)
-    }
-
-    return new Notification(notifications[0], this._helper());
-  }
-
-  async getByIdOfferNotification(id: number) : Promise<Notification> {
-    const notifications = await this._helper().request(`${this._helper().options.url}/offers/notifications`, 'GET', null, {
+    const notifications = await this._helper().request(`${this._helper().options.url}/notifications`, 'GET', null, {
       notification_id : id
     });
 
